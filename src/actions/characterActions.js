9c5835bb4f0d66e3b88dabac1c98bef515cdbd13
ollinieminen <otn987@gmail.com>
2019-05-30 15:08:06 -0700

    done

--- src/actions/characterActions.js
+++ src/actions/characterActions.js
@@ -1,6 +1,14 @@
 import { getCharacters } from '../services/getCharacters';
 
 export const GET_CHARACTERS = 'GET_CHARACTERS';
+
+export const action_getCharacters = () => ({
+  type: GET_CHARACTERS,
+  payload: getCharacters()
+});
+
+
+
 export const action_getCharacters = () => dispatch => {
   return getCharacters()
     .then(results=>{
@@ -11,3 +19,4 @@ export const action_getCharacters = () => dispatch => {
     });
 }
 ;
+
